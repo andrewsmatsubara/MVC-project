84daf58154624339642995d18aad26f8d5cf6258
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-02-18 19:09:45 -0300

    Começando projeto MVC

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+app.use(bodyParser.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Ouvindo na porta ${ process.env.PORT }`);
+});
